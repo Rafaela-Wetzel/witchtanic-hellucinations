a44ebc9cda86c6a37ef6eb651ead696ba828fecf
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2023-11-10 14:41:50 +0000

    Add event listeners and empty functions in js file

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+});
+
+document.getElementById('start').addEventListener('click', startGame);
+
+function startGame() {
+  
+}
+
+function startTimer() {
+
+}
+
+function revealCard() {
+
+}
+
+function hideCard() {
+
+}
+
+function compareCards() {
+
+}
+
+function increaseScore() {
+
+}
+
+function winGame() {
+
+}
+
+function loseGame() {
+
+}
+
+function shuffleCards() {
+
+}
+
+function endGame() {
+    
+}
