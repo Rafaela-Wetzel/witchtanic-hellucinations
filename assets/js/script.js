123cbb824c03eb1fdf7b9ca6582e7d359d801303
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2023-11-12 12:56:28 +0000

    Store cards in variables, set up compareCards function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
+let catFront = document.getElementById('cat-front');
+let catBack = document.getElementById('cat-back');
+
+let broomFront = document.getElementById('broom-front');
+let broomBack = document.getElementById('broom-back');
+
+let cauldronFront = document.getElementById('cauldron-front');
+let cauldronBack = document.getElementById('cauldron-back');
+
+let ballFront = document.getElementById('ball-front');
+let ballBack = document.getElementById('ball-back');
+
+let crystalsFront = document.getElementById('crystals-front');
+let crystalsBack = document.getElementById('crystals-back');
+
+let elixirFront = document.getElementById('elixir-front');
+let elixirBack = document.getElementById('elixir-back');
+
+let incenseFront = document.getElementById('incense-front');
+let incenseBack = document.getElementById('incense-back');
+
+let bookFront = document.getElementById('book-front');
+let bookBack = document.getElementById('book-back');
+
+
+document.addEventListener('DOMContentLoaded', function () {
 
 });
 
@@ -17,15 +42,16 @@ function startGame() {
 function startTimer() {
   let count = 21;
   let countdown = document.getElementById('timer2');
-const timer = setInterval(function () {
-  count--;
-  console.log(count);
-  countdown.innerText = count;
-  if (count === 0) {
-    clearInterval(timer);
-    console.log("Time's up!");
-  }
-}, 1000);
+  const timer = setInterval(function () {
+    count--;
+    console.log(count);
+    countdown.innerText = count;
+    if (count === 0) {
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+
 }
 
 /**
@@ -46,13 +72,43 @@ function hideCard() {
  * 
  */
 function compareCards() {
+  if (catFront === catFront ||
+    broomFront === broomFront ||
+    cauldronFront === cauldronFront ||
+    ballFront === ballFront ||
+    crystals === crystalsFront ||
+    elixirFront === elixirFront ||
+    incenseFront === incenseFront ||
+    bookFront === bookFront
+  ) {
+    matchingCards();
+  } else if (
+    catFront === catFront &&
+    broomFront === broomFront &&
+    cauldronFront === cauldronFront &&
+    ballFront === ballFront &&
+    crystals === crystalsFront &&
+    elixirFront === elixirFront &&
+    incenseFront === incenseFront &&
+    bookFront === bookFront
+  ) {
+    winGame();
+  } else {
+    noMatchingCards();
+  }
+}
+
+/**
+ * 
+ */
+function matchingCards() {
 
 }
 
 /**
  * 
  */
-function increaseScore() {
+function noMatchingCards() {
 
 }
 
@@ -81,5 +137,5 @@ function shuffleCards() {
  * 
  */
 function endGame() {
-    
+
 }
