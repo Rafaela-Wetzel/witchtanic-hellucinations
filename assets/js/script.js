f9b99b349fd5b333be0d6af473c2703d1f8e5ce8
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2023-11-13 18:05:45 +0000

    Adjust size of memory cards, remove borders within memory-game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+/* Source: Tutorial [1]*/
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+function flipCard() {
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        console.log({firstCard, secondCard});
+    }     
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+/* Source end: Tutorial [1]*/
+
+
 document.getElementById('start').addEventListener('click', startGame);
 
 let allCards = document.getElementsByClassName('single-card');
