290398aede075b5f9e04118474337d40a8dfba9c
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2023-11-14 12:20:32 +0000

    Add if else condition to see if the cards match, and get them to cover again if they do not match

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,18 @@ function flipCard() {
         hasFlippedCard = false;
         secondCard = this;
 
-        
+        //do cards match?
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+          // it is a match
+          firstCard.removeEventListener('click', flipCard);
+          secondCard.removeEventListener('click', flipCard);
+        } else {
+          // not a match
+          setTimeout(() => {
+          firstCard.classList.remove('flip');
+          secondCard.classList.remove('flip');
+        }, 1500)
+        }
     }     
 }
 
