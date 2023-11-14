534f440195001dfa6e0ff2e420014c59f356caa6
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2023-11-14 12:43:26 +0000

    Refactor / extract code: create checkForMatch, disableCards and unflipCards functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,33 @@
 
 }); */
 
+document.getElementById('start').addEventListener('click', startGame);
+
+/**
+ * 
+ */
+function startGame() {
+  startTimer();
+}
+
+/** How to create a JavaScript timer by:
+ * https://www.shecodes.io/athena/52336-how-to-create-a-countdown-timer-in-javascript+
+ */
+function startTimer() {
+  let count = 21;
+  let timer = document.getElementById('countdown');
+  const time = setInterval(function () {
+    count--;
+    console.log(count);
+    countdown.innerText = count;
+    if (count === 0) {
+      clearInterval(time);
+      loseGame();
+    }
+  }, 1000);
+
+}
+
 /* Source: Tutorial [1] - Making the cards flip*/
 const cards = document.querySelectorAll('.memory-card');
 
@@ -10,103 +37,42 @@ let firstCard, secondCard;
 
 
 function flipCard() {
-    this.classList.toggle('flip');
+  this.classList.toggle('flip');
 
-    if (!hasFlippedCard) {
-        // first click
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-        // second click
-        hasFlippedCard = false;
-        secondCard = this;
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
 
-        //do cards match?
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-          // it is a match
-          firstCard.removeEventListener('click', flipCard);
-          secondCard.removeEventListener('click', flipCard);
-        } else {
-          // not a match
-          setTimeout(() => {
-          firstCard.classList.remove('flip');
-          secondCard.classList.remove('flip');
-        }, 1500)
-        }
-    }     
+    return;
+  }
+  // second click
+  hasFlippedCard = false;
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500)
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 /* Source end: Tutorial [1]*/
-
-document.getElementById('start').addEventListener('click', startGame);
-
-/**
- * 
- */
-function startGame() {
-  startTimer();
-  compareCards();
-}
-
-/** How to create a JavaScript timer by:
- * https://www.shecodes.io/athena/52336-how-to-create-a-countdown-timer-in-javascript+
- */
-function startTimer() {
-  let count = 21;
-  let countdown = document.getElementById('timer2');
-  const timer = setInterval(function () {
-    count--;
-    console.log(count);
-    countdown.innerText = count;
-    if (count === 0) {
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
-
-}
-
-/**
- * 
- */
-function hideCard() {
-
-}
-
-/**
- * 
- */
-function compareCards() {
-  let faces = document.getElementsByClassName("front-face");
-  for (let i = 0; i < faces.length; i++) {
-    let frontMotif = faces[i].getAttribute("src");
-     
-    }
-    
-}
-  /*if (x) {
-
-  }
-  matchingCards(); 
-  winGame();
-  noMatchingCards();
-}*/
-
-/**
- * 
- */
-function matchingCards() {
-
-}
-
-/**
- * 
- */
-function noMatchingCards() {
-
-}
 
 /**
  * 
@@ -134,4 +100,4 @@ function shuffleCards() {
  */
 function endGame() {
 
-} 
+}
