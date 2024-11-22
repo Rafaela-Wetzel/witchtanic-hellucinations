ade2471edd28f3e001367361f95cd04ffc8db23f
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-11-22 19:47:15 +0000

    Correct error in redirection link to contact confirmation page

--- assets/js/contact-script.js
+++ assets/js/contact-script.js
@@ -5,10 +5,8 @@ window.onload = function () {
         event.preventDefault();
         emailjs.sendForm('service_r17121989w', 'contact_form', this)
             .then(function () {
-                console.log('SUCCESS!');
-                window.location.href = 'https://rafaela-wetzel.github.io/Witchtanic-Hellucinations/contact-confirmation.html';
+                window.location.href = 'https://rafaela-wetzel.github.io/witchtanic-hellucinations/contact-confirmation.html';
             }, function (error) {
-                console.log('FAILED...', error);
                 alert('Unfortunately your message has not been sent due to an error. Please try again.');
             });
     });
